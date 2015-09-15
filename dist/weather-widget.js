72262d299acecc50c80cef6947c445ca513c7e4f
Matt O'Connell <matt@bluebite.com>
2015-09-15 12:28:54 -0400

    create widget and style

--- dist/weather-widget.js
+++ dist/weather-widget.js
@@ -12,7 +12,7 @@
 	 */
 	var WeatherWidget = function(el, customOptions) {
 		var _ = this;
-		_.$el = $(el).addClass('weather-grid');
+		_.$el = $(el).addClass('weather-widget');
 		_.defaults = defaults;
 
 		_.init = function() {
@@ -70,14 +70,73 @@
 		});
 	};
 
+	var days = [
+		'Sunday',
+		'Monday',
+		'Tuesday',
+		'Wednesday',
+		'Thursday',
+		'Friday',
+		'Saturday'
+	];
+
 	WeatherWidget.prototype.makeWidget = function(apiData) {
-		console.log(apiData);
-		var currently = apiData.currently;
+		var currently = apiData.currently,
+			hourly = apiData.hourly.data,
+			daily = apiData.daily.data;
 		var _ = this;
-		var header = '<h1>' + currently.temperature + ' degrees</h1>';
-		header += '<h2>' + currently.summary + '</h2>';
-		var $header = $(header);
-		_.$el.append($header);
+		var $header = $(
+			'<h1>' + currently.temperature + '&deg;</h1>' +
+			'<h2>' + currently.summary + '</h2>'
+		);
+		var $hourly = $('<div class="hourly"></div>');
+		var $daily = $('<div class="daily"></div>');
+
+
+		for(var i = 0; i < 5; i++) {
+			var temp = Math.round(hourly[i+1].temperature),
+				icon = hourly[i+1].icon,
+				hour = (new Date()).getHours() + i,
+				ampm = hour >= 12 ? 'pm' : 'am';
+			if (hour > 23) {
+				hour = Math.abs(23 - hour);
+				ampm = 'am';
+			}
+			hour = hour % 12;
+			hour = hour ? hour : 12;
+
+			$hourly
+				.append($(
+					'<div><span class="time">' + hour + ampm + '</span>' +
+					'<span class="icon ' + icon + '"></span>' +
+					'<span class="temp">' + temp + '&deg;</span></div>'
+				));
+		}
+
+		$daily.append($(
+			'<div class="daily-title">' +
+			'<span>7 Day Forecast</span><span>High</span><span>Low</span>' +
+			'</div>'
+		));
+		for(var k = 0; k < 7; k++) {
+			var day = days[new Date(daily[k].time * 1000).getDay()],
+				icon = daily[k].icon,
+				max = Math.round(daily[k].temperatureMax),
+				min = Math.round(daily[k].temperatureMin);
+			$daily
+				.append($(
+					'<div><span class="day">' + day + '</span>' +
+					'<span class="icon ' + icon + '"></span>' +
+					'<span class="max">' + max + '&deg;</span>' +
+					'<span class="min">' + min + '&deg;</span></div>'
+				));
+		}
+		var $widget = _.$el
+			.append($header)
+			.append($hourly)
+			.append($daily);
+
+		_.$el.append($widget);
 	};
 
 	// Extend JQuery fn for $('$id').weatherWidget()
